e7ad4cd5a63aa0cf31efb731ec31410fd9f026d0
HimanshuPhoenix <141736130+HimanshuPhoenix@users.noreply.github.com>
2025-08-29 22:09:54 +0530

    Update mysql_queries.sql

--- mysql_queries.sql
+++ mysql_queries.sql
@@ -1,7 +1,13 @@
 create database AIProject;
 use AIProject;
 show tables;
-desc events;
+/*desc events;
 select * from events;
 select count(*) as NetCnt from events union
 select count(*) as NoLocationCnt from events where location_x=0;
+Created new table events1 with file_name column*/
+desc events1;
+select * from events1;
+select count(*) as NetCnt from events1 union
+select count(*) as NoLocationCnt from events1 where location_x=0;
+select count(*) as FileCnt from (select count(*) as FileRecCnt from events1 group by file_name) as t;
