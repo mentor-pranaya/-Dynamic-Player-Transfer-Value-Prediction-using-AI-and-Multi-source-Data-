590b68d396cc24549bb7f7987694d186ec0b4812
HimanshuPhoenix <141736130+HimanshuPhoenix@users.noreply.github.com>
2025-09-19 12:33:05 +0530

    Create enocding.sql

---
+++ sql/enocding.sql
@@ -0,0 +1,13 @@
+update player_injuries_trfrmrkt set games_missed=0 where games_missed is null;
+alter table player_transfer_history add column mv_raw varchar(50) after market_value;
+alter table player_transfer_history add column fee_raw varchar(50) after fee;
+create table player_transfer_history_18sep select * from player_transfer_history;
+delete from player_transfer_history;
+delete from clubs_trfrmrkt;
+ALTER TABLE clubs_trfrmrkt AUTO_INCREMENT = 1;
+insert into clubs_trfrmrkt (name) (select distinct club from transfermrkt_new18sep order by club);
+alter table transfermrkt_new18sep add column clubid int, add column compid int;
+update transfermrkt_new18sep p, clubs_trfrmrkt t set p.clubid=t.id where t.name = p.club;
+update transfermrkt_new18sep p, competitions_trfrmrkt t set p.compid=t.id where t.name = p.competition;
+select count(*) from transfermrkt_new18sep where clubid is null;
+select count(*) from transfermrkt_new18sep where compid is null;
